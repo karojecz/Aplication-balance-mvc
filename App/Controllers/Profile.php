a9e4dd0fc878d3b9d5aefb9548ce26a1034186d2
Karol <karojecz@gmail.com>
2022-06-27 22:51:29 +0200

    fix edit and delete bug

--- App/Controllers/Profile.php
+++ App/Controllers/Profile.php
@@ -29,11 +29,12 @@ class Profile extends Authenticated
 		'user'=>$this->user
 		]);
 	}
-	public function editExpenseCategoryAction() 
+	 
+	public function editCategory($name) 
 	{
 	if (isset($_POST['new'])) {
-		
-			View::renderTemplate('Profile/EditExpense.html');
+	
+			View::renderTemplate('Profile/Add'.$name.'Category.html');
 	
 		
 	
@@ -41,39 +42,60 @@ class Profile extends Authenticated
 		
 		if(isset($_POST['category'])){
 			$_SESSION['name_to_edit']=$_POST['category'];
-			View::renderTemplate('Profile/testExpense.html');
+			View::renderTemplate('Profile/edit'.$name.'.html');
 			
 		}else{
 			Flash::addMessage('Selecet category first', Flash::WARNING);
-			$this->redirect('/profile/ExpenseCategory');
+			$this->redirect('/profile/'.$name.'Category');
 		}
     
 	} else if(isset($_POST['delete'])){
 		if(isset($_POST['category'])){
 			
-			Profile::deleteExpensesCategorys();
+			Profile::deleteCategorys($name);
 			Flash::addMessage('Item removed');
-			$this->redirect('/profile/ExpenseCategory');
+			$this->redirect('/profile/'.$name.'Category');
 			
 		}else{
 			Flash::addMessage('Selecet category first', Flash::WARNING);
-			$this->redirect('/profile/ExpenseCategory');
+			$this->redirect('/profile/'.$name.'Category');
 		}
     
 	}
+	}
+	public function deleteCategorys($name)
+	{
+		$items=BalanceModel::getCategorys(''.$name.'s_category_assigned_to_users');
+		if(count($items)>1)
+		{
+		$data=$_POST['category'];
+		
+		BalanceModel::delete_category(''.$name.'s_category_assigned_to_users',$data);
+		
+
+		}else{
+			Flash::addMessage('Cant delete last item ',Flash::WARNING);
+			$this->redirect('/profile/'.$name.'Category');
+		}
 		
 	}
+	
+	public function editExpenseCategoryAction()
+	{
+		Profile::editCategory('Expense');
+	}
+
 	public function ExpenseCategoryAction()
 	{
-		View::renderTemplate('Profile/ExpenseCategory.html',[
+		View::renderTemplate('Profile/EditCategory.html',[
 		'user'=>$this->user,
-		'categorys'=>BalanceModel::getCategorys('expenses_category_assigned_to_users')
+		'categorys'=>BalanceModel::getCategorys('expenses_category_assigned_to_users'),
+		'action_title'=>'EditExpenseCategory',
+		'title'=>'Expense'
 		]);
 	}
 	public function AddExpenseCategoryAction()
 	{
-		
-		
 		$data=$_POST['category'];
 		
 		if(BalanceModel::check_if_category_exist('expenses_category_assigned_to_users',$data)){
@@ -92,48 +114,10 @@ class Profile extends Authenticated
 			$this->redirect('/profile/ExpenseCategory');
 		}
 	}
-	public function deleteExpensesCategorys()
-	{
-		$items=BalanceModel::getCategorys('expenses_category_assigned_to_users');
-		if(count($items)>1)
-		{
-		$data=$_POST['category'];
-		
-		BalanceModel::delete_category('expenses_category_assigned_to_users',$data);
-		
 
-		}else{
-			Flash::addMessage('Cant delete last item ',Flash::WARNING);
-			$this->redirect('/profile/ExpenseCategory');
-		}
-		
-	}
-	public function DeleteExpenseAction()
-	{
-		View::renderTemplate('Profile/DeleteExpense.html',[
-		'user'=>$this->user,
-		'categorys'=>BalanceModel::getCategorys('expenses_category_assigned_to_users')
-		]);
-	}
-	public function editExpenseAction()
-	{
-		
-		View::renderTemplate('Profile/EditExpense.html',[
-		'user'=>$this->user,
-		'categorys'=>BalanceModel::getCategorys('expenses_category_assigned_to_users')
-		]);
-		
-	}
-	/*
-	public function editExpensesCategorysAction()
-	{
-		$_SESSION['name_to_edit']=$_POST['category'];
-		View::renderTemplate('Profile/testExpense.html');
-		
-		
-		
-	}
-	*/
+
+
+
 	public function saveEditExpenseCategorysAction()
 	{
 		if(BalanceModel::check_if_category_exist('expenses_category_assigned_to_users',$_POST['category'])){
@@ -150,12 +134,17 @@ class Profile extends Authenticated
 	public function IncomeCategoryAction()
 	{
 		
-		//BalanceModel::check_if_category_exist('incomes_category_assigned_to_users',$);
-		
-		View::renderTemplate('Profile/IncomeCategory.html',[
-		'user'=>$this->user
+		View::renderTemplate('Profile/EditCategory.html',[
+		'user'=>$this->user,
+		'categorys'=>BalanceModel::getCategorys('incomes_category_assigned_to_users'),
+		'action_title'=>'editIncomesCategory',
+		'title'=>'Income'
 		]);
 		
+	}
+	public function editIncomesCategoryAction()
+	{
+		Profile::editCategory('Income');
 	}
 	public function AddIncomeCategoryAction()
 	{
@@ -181,7 +170,7 @@ class Profile extends Authenticated
 		}
 			
 	}
-	public function deleteIncomeCategorysAction()
+	public function deleteIncomeCategorys()
 	{
 		$items=BalanceModel::getCategorys('incomes_category_assigned_to_users');
 		if(count($items)>1)
@@ -198,13 +187,19 @@ class Profile extends Authenticated
 		}
 		
 	}
-	public function DeleteIncomeAction()
+	public function saveEditIncomesCategorysAction ()
 	{
-		View::renderTemplate('Profile/DeleteIncome.html',[
-		'user'=>$this->user,
-		'categorys'=>BalanceModel::getCategorys('incomes_category_assigned_to_users')
-		]);
+		if(BalanceModel::check_if_category_exist('incomes_category_assigned_to_users',$_POST['category'])){
+		BalanceModel::setNewName('incomes_category_assigned_to_users',$_SESSION['name_to_edit'],$_POST['category']);
+		$_SESSION['CATEGORY_TO_EDIT']="";
+		Flash::addMessage('Name changed');
+		$this->redirect('/profile/show');
+		}	else{
+			Flash::addMessage('this category alerady exist', Flash::WARNING);
+			$this->redirect('/profile/IncomeCategory');
+		}
 	}
+
 	public function PaymentCategoryAction()
 	{
 		View::renderTemplate('Profile/PaymentCategory.html',[
