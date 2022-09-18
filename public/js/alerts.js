50a8addee9d803dcce1f74dc2aef9f833e2c9aad
Karol <karojecz@gmail.com>
2022-09-18 13:16:32 +0200

    text area validation in js expense and income

--- public/js/alerts.js
+++ public/js/alerts.js
@@ -1,6 +1,39 @@
 
 				
-				
+				//fade main alerts from flash controler
 				setTimeout(function () {
 				$( "div[name='alerts_flash']" ).hide('fade');
 				},2000);
+				
+				
+				
+				
+				//this is js validation of text area in income and expense file
+				
+
+document.getElementById("submit_button").addEventListener("click", function(event){
+
+//var comment=document.getElementById('Textarea').value;
+var comment=document.getElementsByName("comment").value;
+
+const regex=/^[A-Za-z0-9 ,.]*$/;
+
+if(!regex.test(comment)){
+	
+	
+
+	document.getElementById("text_area_alert").innerHTML = "You can only use letters and numbers. Special characters are not allowed." ;
+	$('#text_area_alert').show();
+	
+	
+	setTimeout(function () {
+				$( "#text_area_alert" ).hide('fade');
+				},3000);
+	
+  event.preventDefault();
+  
+  }
+  
+});
+				
+				
