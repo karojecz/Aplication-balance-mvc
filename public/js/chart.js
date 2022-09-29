57243fc1e71b375c8cd73e06e6b8685acf20e489
Karol <karojecz@gmail.com>
2022-09-29 22:14:26 +0200

    handle no data case in expense piechart

--- public/js/chart.js
+++ public/js/chart.js
@@ -9,33 +9,37 @@ google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
 
-var data = new google.visualization.DataTable();
 
-data.addColumn('string', 'Category name');
-data.addColumn('number', 'Sum');
-		for (var key in myArr) {
-    var value = myArr[key];
-		var sum=parseFloat(value.sum);
-	data.addRow([value.name,sum]);
-}
+	if (Array.isArray(myArr) && myArr.length){
+						var data = new google.visualization.DataTable();
 
-
-
-  // Optional; add a title and set the width and height of the chart
-  var options = {
-	  backgroundColor: 'transparent',
-	  legend: {position: 'top', textStyle: {color: 'white', fontSize: 15}},
-	  title:"Your expenses" ,titleTextStyle: {color: 'white', fontSize: 19,},
-	     
-	  };
-
-  // Display the chart inside the <div> element with id="piechart"
-  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-  chart.draw(data, options);
-  
-  
+						data.addColumn('string', 'Category name');
+						data.addColumn('number', 'Sum');
 
 	
+								for (var key in myArr) {
+							var value = myArr[key];
+								var sum=parseFloat(value.sum);
+							data.addRow([value.name,sum]);
+						}
+
+
+
+						  // Optional; add a title and set the width and height of the chart
+						  var options = {
+							  backgroundColor: 'transparent',
+							  legend: {position: 'top', textStyle: {color: 'white', fontSize: 15}},
+							  title:"Your expenses" ,titleTextStyle: {color: 'white', fontSize: 19,},
+								 
+							  };
+
+						  // Display the chart inside the <div> element with id="piechart"
+						  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+						  chart.draw(data, options);
+	}
+						  
+
+							
 }
     $(window).resize(function(){
         drawChart();
