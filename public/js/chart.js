61f563957400148963e39741d965f9719de62108
Karol <karojecz@gmail.com>
2022-10-01 21:03:25 +0200

    add income pie chart to balance.html

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,6 +1,7 @@
 // Load google c
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawChart2);
 
 // Draw the chart and set the chart values
 
@@ -30,6 +31,8 @@ function drawChart() {
 							  backgroundColor: 'transparent',
 							  legend: {position: 'top', textStyle: {color: 'white', fontSize: 15}},
 							  title:"Your expenses" ,titleTextStyle: {color: 'white', fontSize: 19,},
+
+							
 								 
 							  };
 
@@ -41,6 +44,47 @@ function drawChart() {
 
 							
 }
+
+	
+	
+	function drawChart2() {
+
+
+	if (Array.isArray(myArr) && myArr.length){
+						var data = new google.visualization.DataTable();
+
+						data.addColumn('string', 'Category name');
+						data.addColumn('number', 'Sum');
+
+	
+								for (var key in myArr) {
+							var value = myArr[key];
+								var sum=parseFloat(value.sum);
+							data.addRow([value.name,sum]);
+						}
+
+
+
+						  // Optional; add a title and set the width and height of the chart
+						  var options = {
+							  backgroundColor: 'transparent',
+							  legend: {position: 'top', textStyle: {color: 'white', fontSize: 15}},
+							  title:"Your expenses" ,titleTextStyle: {color: 'white', fontSize: 19,},
+
+								 
+							  };
+
+						  // Display the chart inside the <div> element with id="piechart"
+						  var chart = new google.visualization.PieChart(document.getElementById('piechart2'));
+						  chart.draw(data, options);
+	}
+						  
+
+							
+}
+	
     $(window).resize(function(){
-        drawChart();
+        drawChart2();
+		 drawChart();
     });
+	
