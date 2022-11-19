455dce6884e2a5c43d72e300d9208dc4fbda3550
Karol <karojecz@gmail.com>
2022-11-19 11:56:50 +0100

    add limit.js file

---
+++ public/js/limit.js
@@ -0,0 +1,11 @@
+
+	function limitTEST(){
+		
+		console.log('ok');
+		let id=10;
+		let token=46;
+		fetch(`/Balance/testFetch/${id}?name=46`)
+		.then((response)=>response.json())
+		.then((data)=>console.log(data))
+		
+	}
